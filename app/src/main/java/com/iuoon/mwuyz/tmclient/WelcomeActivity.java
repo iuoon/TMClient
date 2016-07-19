4c3aa2d1f685a8d4ff672512e63bafacd9efd225
mwuyz <mwuyz@outlook.com>
2016-07-19 17:11:15 +0800

    欢迎界面修改

--- app/src/main/java/com/iuoon/mwuyz/tmclient/WelcomeActivity.java
+++ app/src/main/java/com/iuoon/mwuyz/tmclient/WelcomeActivity.java
@@ -13,7 +13,6 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         /**标题是属于View的，所以窗口所有的修饰部分被隐藏后标题依然有效,需要去掉标题**/
         //   requestWindowFeature(Window.FEATURE_NO_TITLE);
