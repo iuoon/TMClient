15c8af9bdd368e5f01d78c846911810f77024f5e
mwuyz <mwuyz@outlook.com>
2016-07-28 12:17:45 +0800

    增加网络访问权限，修改欢迎背景界面，修改标题栏

--- app/src/main/java/com/iuoon/mwuyz/tmclient/WelcomeActivity.java
+++ app/src/main/java/com/iuoon/mwuyz/tmclient/WelcomeActivity.java
@@ -1,6 +1,7 @@
 package com.iuoon.mwuyz.tmclient;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
